4ca5d406d4845854c46fe9a818730a89ca0d1f7a
Christos Nicolaou <chrisanicolaou>
2022-05-02 13:33:35 +0100

    Sets up example screen component

---
+++ src/GetActiveScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const GetActiveScreen = () => {
+  return (
+    <View>
+      <Text>GetActiveScreen</Text>
+    </View>
+  );
+};
+
+export default GetActiveScreen;
