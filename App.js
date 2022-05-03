75fdd53d16a27b751b5f3706b9bd39f832259e6a
Christos Nicolaou <chrisanicolaou>
2022-05-03 10:35:57 +0100

    Adds react-navigation package, sets up screen stack in App.js. Includes small test in LoginScreen.js

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./src/screens/LoginScreen";
+import HomepageScreen from "./src/screens/HomepageScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import PickInterestsScreen from "./src/screens/PickInterestsScreen";
+import HobbySwipeScreen from "./src/screens/HobbySwipeScreen";
+import GetActiveScreen from "./src/screens/GetActiveScreen";
+import MeditateScreen from "./src/screens/MeditateScreen";
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.root}>
-      <LoginScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={styles.root}>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Homepage" component={HomepageScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="PickInterests" component={PickInterestsScreen} />
+          <Stack.Screen name="HobbySwipe" component={HobbySwipeScreen} />
+          <Stack.Screen name="GetActive" component={GetActiveScreen} />
+          <Stack.Screen name="Meditate" component={MeditateScreen} />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
