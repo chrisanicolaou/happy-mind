10189c0e32e03f367d567451545ebd4d879b9938
Christos Nicolaou <chrisanicolaou>
2022-05-04 19:34:38 +0100

    Changes initial route back to LoginScreen on App.js

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const App = () => {
       <UserContext.Provider value={{ user, setUser }}>
         <SafeAreaView style={styles.root}>
           <Stack.Navigator
-            initialRouteName="Homepage"
+            initialRouteName="Login"
             screenOptions={{ headerShown: false }}
           >
             <Stack.Screen name="Login" component={LoginScreen} />
