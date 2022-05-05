c638df26ca2b39e551fe0261776b5182a10f48d8
kawla95 <kawlamansur@hotmail.co.uk>
2022-05-05 16:17:32 +0100

    added link to an image

--- src/screens/MeditateScreen.jsx
+++ src/screens/MeditateScreen.jsx
@@ -3,10 +3,25 @@ import React from "react";
 
 const MeditateScreen = () => {
   return (
-    <View>
-      <Text>MeditateScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={Styles.detailsImageWrapper}>
+        <Image
+          style={{ width: "100%", height: "50%" }}
+          source={{
+            uri: "https://images.unsplash.com/photo-1508672019048-805c876b67e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVkaXRhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+          }}
+        />
+        <Text>MeditateScreen</Text>
+      </View>
+    </ScrollView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+  },
+  detailsImageWrapper: {},
+});
 
 export default MeditateScreen;
