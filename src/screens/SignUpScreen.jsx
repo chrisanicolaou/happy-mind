c3d9d36c5ef2eac985dfc3e210f0d08769c72af9
Andrew Torr <andrewtorr1@outlook.com>
2022-05-06 13:24:37 +0100

    updated styling for login and signup

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -144,7 +144,7 @@ const styles = StyleSheet.create({
   root: {
     alignItems: "center",
     padding: 60,
-    backgroundColor: "rgba(-20, 160, 200, 0.4)",
+    backgroundColor: "rgba(rgba(100, 20, 200, 0.4))",
   },
   logo: {
     width: "90%",
