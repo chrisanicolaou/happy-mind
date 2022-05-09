c0651fbcf575bf88a7d2eb2892567e83bd1c8873
Christos Nicolaou <chrisanicolaou>
2022-05-09 01:19:39 +0100

    IMPORTANT: run npm install ---- restyles Login page to use React Native Paper components

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -7,8 +7,10 @@ import {
   useWindowDimensions,
   ScrollView,
   Dimensions,
+  SafeAreaView,
+  KeyboardAvoidingView,
 } from "react-native";
-import Logo from "../../images/Logo.jpeg";
+import Logo from "../../assets/logo.png";
 import CustomInput from "../components/CustomInput";
 import CustomButton from "../components/CustomButton";
 
@@ -19,13 +21,19 @@ import { getDoc, doc } from "firebase/firestore";
 import { useNavigation } from "@react-navigation/native";
 import { UserContext } from "../utils/UserContext";
 import { loginUser } from "../utils/api";
+import { Button, HelperText, TextInput } from "react-native-paper";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const LoginScreen = () => {
   const [email, setEmail] = useState(""); //update from [username, setUsername]
   const [password, setPassword] = useState("");
-  const [emailOrPassError, setEmailOrPassError] = useState("");
+  const [emailOrPassError, setEmailOrPassError] = useState({
+    type: "error" || "info",
+    visible: false,
+    message: "",
+  });
 
-  const { height } = useWindowDimensions();
+  // const { height } = useWindowDimensions();
   const navigation = useNavigation();
   const { setUser } = useContext(UserContext);
 
@@ -33,19 +41,31 @@ const LoginScreen = () => {
     //Refactored - see api.js for details
     try {
       if (!email) {
-        setEmailOrPassError("Please enter an email address.");
+        setEmailOrPassError({
+          type: "error",
+          visible: true,
+          message: "Please enter an email address.",
+        });
         return;
       }
       if (!password) {
-        setEmailOrPassError("Please enter a password.");
+        setEmailOrPassError({
+          type: "error",
+          visible: true,
+          message: "Please enter a password.",
+        });
         return;
       }
       const user = await loginUser(email, password);
-      setUser(user);
       setEmail("");
       setPassword("");
+      setUser(user);
     } catch (err) {
-      setEmailOrPassError(err.message);
+      setEmailOrPassError({
+        type: "error",
+        visible: true,
+        message: err.message,
+      });
     }
 
     //----------OLD CODE BELOW----------
@@ -68,73 +88,113 @@ const LoginScreen = () => {
     navigation.navigate("ForgotPassword");
   };
 
-  const onLoginFacebook = () => {};
+  // const onLoginFacebook = () => {};
 
-  const onLoginGoogle = () => {};
+  // const onLoginGoogle = () => {};
 
   const onSignUpPressed = () => {
     navigation.navigate("SignUp");
   };
 
   return (
-    <ScrollView>
-      <View style={styles.root}>
-        <Image
-          source={Logo}
-          style={[styles.logo, { height: height * 0.3 }]}
-          resizeMode="contain"
-        />
-        <>{emailOrPassError ? <Text> {emailOrPassError} </Text> : null}</>
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAwareScrollView>
+        <View style={styles.root}>
+          <Image source={Logo} style={styles.logo} />
+          <HelperText
+            type={emailOrPassError.type}
+            visible={emailOrPassError.visible}
+            style={{ alignSelf: "center" }}
+          >
+            {emailOrPassError.message}
+          </HelperText>
+          <TextInput
+            label="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={styles.textInput}
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            style={styles.textInput}
+            secureTextEntry={true}
+          />
+          <Button
+            mode="contained"
+            style={styles.button}
+            onPress={onLoginPressed}
+          >
+            Login
+          </Button>
+          <Button mode="text" onPress={onForgotPasswordPressed} color="red">
+            Forgot Password?
+          </Button>
+          <Button mode="text" onPress={onSignUpPressed}>
+            Don't have an account? Sign Up
+          </Button>
+          {/* <>{emailOrPassError ? <Text> {emailOrPassError} </Text> : null}</>
 
-        <CustomInput placeholder="Email" value={email} setValue={setEmail} />
-        <CustomInput
-          placeholder="Password"
-          value={password}
-          setValue={setPassword}
-          secureTextEntry={true}
-        />
-        <CustomButton text="Sign In" onPress={onLoginPressed} />
+<CustomInput placeholder="Email" value={email} setValue={setEmail} />
+<CustomInput
+placeholder="Password"
+value={password}
+setValue={setPassword}
+secureTextEntry={true}
+/>
+<CustomButton text="Sign In" onPress={onLoginPressed} />
 
-        <CustomButton
-          text="Forgot password?"
-          onPress={onForgotPasswordPressed}
-          type="TERTIARY"
-        />
-        <CustomButton
-          text="Sign In with Facebook"
-          onPress={onLoginFacebook}
-          bgColor="#E7EAF4"
-          fgColor="#4765A9"
-        />
-        <CustomButton
-          text="Sign In with Google"
-          onPress={onLoginGoogle}
-          bgColor="#FAE9EA"
-          fgColor="#DD4D44"
-        />
+<CustomButton
+text="Forgot password?"
+onPress={onForgotPasswordPressed}
+type="TERTIARY"
+/>
+<CustomButton
+text="Sign In with Facebook"
+onPress={onLoginFacebook}
+bgColor="#E7EAF4"
+fgColor="#4765A9"
+/>
+<CustomButton
+text="Sign In with Google"
+onPress={onLoginGoogle}
+bgColor="#FAE9EA"
+fgColor="#DD4D44"
+/>
 
-        <CustomButton
-          text="Don't have an account? Create one"
-          onPress={onSignUpPressed}
-          type="TERTIARY"
-        />
-      </View>
-    </ScrollView>
+<CustomButton
+text="Don't have an account? Create one"
+onPress={onSignUpPressed}
+type="TERTIARY"
+/> */}
+        </View>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-    alignItems: "center",
-    padding: 40,
-    backgroundColor: "rgba(100, 20, 200, 0.4)",
+    justifyContent: "flex-start",
+    paddingTop: 30,
     height: Dimensions.get("window").height,
     width: Dimensions.get("window").width,
   },
   logo: {
-    width: "90%",
-    maxWidth: "300%",
-    maxHeight: "200%",
+    height: 250,
+    width: 300,
+    alignSelf: "center",
+  },
+  textInput: {
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  button: {
+    width: "75%",
+    alignSelf: "center",
+    marginTop: "2%",
+    marginBottom: "5%",
   },
 });
 
